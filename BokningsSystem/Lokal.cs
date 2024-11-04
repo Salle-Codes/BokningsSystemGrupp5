4db7d161d4aeeb34716153017abcb0d3b224298d
Victor Sahlin <victorsahlin@hotmail.com>
2024-11-04 15:46:01 +0100

    La till metoden Delete i klassen Lokal för att ta bort lokaler från listan.

--- BokningsSystem/Lokal.cs
+++ BokningsSystem/Lokal.cs
@@ -12,7 +12,7 @@ namespace BokningsSystem
 
         public byte Seats { get; set; }
         public byte Outlets { get; set; }
-        public bool Ac {  get; set; }
+        public bool Ac { get; set; }
         public DateTime FreeTimeStart { get; set; }
         public DateTime FreeTimeStop { get; set; }
         public Lokal(string name, byte seats, byte outlets, bool ac, DateTime freetimeStart, DateTime freetimeStop)
@@ -25,20 +25,35 @@ namespace BokningsSystem
             FreeTimeStop = freetimeStop;
         }
 
-            //Patrik gör List, New och Delete-metoderna, Kolla med Ahmed gällande datetime för listan
+        //Patrik gör List, New och Delete-metoderna, Kolla med Ahmed gällande datetime för listan
         public static void List()
         {
             //Listar upp alla salar/grupprum med * om den är upptagen samt egenskaper på rummen
         }
 
-        public static void New ()
+        public static void New()
         {
             //skapa nya salar/rum
         }
 
-        public static void Delete()
+        public static void Delete(List<Lokal> lokaler)
         {
-            //Ta bort sal/rum
+            //Ta bort lokal
+            Console.WriteLine("Vilken lokal vill du ta bort?");
+            var lokalen = Console.ReadLine();
+            //Leta efter matchande lokal i listan och tilldela den till en ny variabel
+            var lokalDelete = lokaler.FirstOrDefault(lok => lok.Name.Equals(lokalen, StringComparison.OrdinalIgnoreCase));
+
+            if (lokalDelete != null)
+            {
+                // Ta bort lokalen från listan
+                lokaler.Remove(lokalDelete);
+                Console.WriteLine($"Lokal '{lokalen}' har tagits bort.");
+            }
+            else
+            {
+                Console.WriteLine($"Lokalen {lokalen} hittades inte.");
+            }
         }
     }
 }
