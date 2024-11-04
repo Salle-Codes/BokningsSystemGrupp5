485bee6bb63e58e62b6d8e53f4978682c129d0da
Hampus Sandström <sandstroms.hampus@gmail.com>
2024-11-04 10:17:04 +0100

    La till metoder i lokalklassen

--- BokningsSystem/Lokal.cs
+++ BokningsSystem/Lokal.cs
@@ -8,6 +8,23 @@ namespace BokningsSystem
 {
     internal class Lokal
     {
+        public string Name { get; set; }
 
+        public byte Seats { get; set; }
+
+        public void List()
+        {
+            //Listar upp alla salar/grupprum med * om den är upptagen samt egenskaper på rummen
+        }
+
+        public void New ()
+        {
+            //skapa nya salar/rum
+        }
+
+        public void Delete()
+        {
+            //Ta bort sal/rum
+        }
     }
 }
